bf9f2e493c2bdaf8c8cdc34e0bf8f6e1a2293944
Sabique Mattummal <119643024+sabique-mattummal@users.noreply.github.com>
2022-12-07 21:20:46 +0530

    Create row_column_marker.py

---
+++ row_column_marker.py
@@ -0,0 +1,28 @@
+#Rows
+
+row1 = ["⬜️","️⬜️","️⬜️"]
+row2 = ["⬜️","⬜️","️⬜️"]
+row3 = ["⬜️️","⬜️️","⬜️"]
+
+#Arrangement
+
+map = [row1, row2, row3]
+
+print(f"{row1}\n{row2}\n{row3}")
+
+#Input
+
+print("The positions are\n[11,21,31]\n[12,22,32]\n[13,23,33]")
+
+position = input("Where do you want to put the marker "X" as per the above arrangemnt ? ")
+
+
+#Position Marking
+
+h = int(position[0]) 
+v = int (position[1]) 
+
+selected_row = map[v - 1]
+selected_row[h-1] = 'X'
+
+print(f"{row1}\n{row2}\n{row3}")
