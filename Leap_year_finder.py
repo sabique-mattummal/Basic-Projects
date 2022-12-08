9beb946e1eaee6ed74471f5fd914176aaec2c5c7
Sabique Mattummal <119643024+sabique-mattummal@users.noreply.github.com>
2022-12-08 08:08:55 +0530

    Update Leap_year_finder.py

--- Leap_year_finder.py
+++ Leap_year_finder.py
@@ -1,14 +1,14 @@
+#Input
 year = int(input("Which year do you want to check? "))
 
+#Brain
 if year % 4 == 0:
     if year % 100 == 0:
         if year % 400 == 0:
-            print("leap year.")
+            print("Its a leap year.")
         else:
-            print("not leap year")
+            print("not a leap year")
     else:
-        print("leap year.")
+        print("Its a leap year.")
 else:
-    print("Not leap year.")
-
-
+    print("Not a leap year.")
