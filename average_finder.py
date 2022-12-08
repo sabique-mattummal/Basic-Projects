d59883c01f3f2416d3a178f624cdc99969d12a9c
Sabique Mattummal <119643024+sabique-mattummal@users.noreply.github.com>
2022-12-08 08:11:48 +0530

    Update average_finder.py

--- average_finder.py
+++ average_finder.py
@@ -1,23 +1,21 @@
 #Input
-student_heights = input("Input a list of student heights, (seperated by a space or tab only) :  ").split()
+numbers = input("Input a list of numbers, (seperated by a space or tab only) :  ").split()
 
 #Conversion of each value in list to an integer value in order to make mathematical oprations
-for n in range(0, len(student_heights)):
-  student_heights[n] = int(student_heights[n])
+for n in range(0, len(numbers)):
+  numbers[n] = int(numbers[n])
 
 #Brain
 total_height = 0
-for height in student_heights:
+for height in numbers:
     total_height += height
 number = 0
-for student in student_heights:
-    number += 1 
+for student in numbers:
+    number += 1
 average = round(total_height / number,1)
 
 #Output
-print(f"The average of {len(student_heights)} students is {average}")
-
-
+print(f"The average of {len(numbers)} numbers is {average}")
 
 
 
