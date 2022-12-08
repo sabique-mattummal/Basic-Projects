2282adff0f2b1705c1dd3ad64b9a5bd29ee7b7d8
Sabique Mattummal <119643024+sabique-mattummal@users.noreply.github.com>
2022-12-08 10:36:12 +0530

    Create basic_cipher_logic.py

---
+++ basic_cipher_logic.py
@@ -0,0 +1,21 @@
+#data
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+#Input
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#Cipher function
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for letter in start_text:
+    position = alphabet.index(letter)
+    new_position = position + shift_amount
+    end_text += alphabet[new_position]
+  print(f"{direction}d result: {end_text}")
+
+  #Console
+caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
