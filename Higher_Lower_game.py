1a066d9a0333778b0cce75a1fa9ceb501ce222c2
Sabique Mattummal <119643024+sabique-mattummal@users.noreply.github.com>
2022-12-09 09:34:31 +0530

    Create Higher_Lower_game.py

---
+++ Higher_Lower_game.py
@@ -0,0 +1,63 @@
+import random
+from data import data
+
+#Function picking a random function
+
+def get_random_account():
+   return random.choice(data)
+
+#Function formatting the data for printable format
+
+def format_data(account):
+    name = account["name"]
+    description = account["description"]
+    country = account["country"]
+    
+    return f"{name}, a {description}, from {country}"
+
+#Function checking answer
+
+def check_answer(guess, a_followers, b_followers):
+
+  if a_followers > b_followers:
+        return guess == "a"
+    else:
+        return guess == "b"
+      
+#Game engine
+
+def game():
+    print("Higher\nVs\nLower")
+    score = 0
+    game_should_continue = True
+    account_a = get_random_account()
+    account_b = get_random_account()
+
+    #account changing trick
+    while game_should_continue:
+        account_a = account_b
+        account_b = get_random_account()
+
+        while account_a == account_b:
+            account_b = get_random_account()
+
+        print(f"Compare A: {format_data(account_a)}.")
+        print("vs")
+        print(f"Against B: {format_data(account_b)}.")
+
+        guess = input("Who has more followers? Type 'A' or 'B': \n").lower()
+        a_follower_count = account_a["follower_count"]
+        b_follower_count = account_b["follower_count"]
+        is_correct = check_answer(guess, a_follower_count, b_follower_count)
+        
+       if is_correct:
+            score += 1
+            print(f"You're right! Current score: {score}.")
+        else:
+            game_should_continue = False
+            print(f"Sorry, that's wrong. Final score: {score}")
+
+#Console
+game()
+while input("Play Again ? Type 'y'\n") == 'y':
+    game()
