8137ec5c273e0845d0f6452fce9dc7ccacd9e167
Sabique Mattummal <119643024+sabique-mattummal@users.noreply.github.com>
2022-12-10 09:52:45 +0530

    Update main.py

--- Hangman/main.py
+++ Hangman/main.py
@@ -1,39 +1,42 @@
 
 import random
 from hangman_words import word_list
+from hangman_art import stages
 
+#Picking a random word 
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 
+
+#Game initialisation
 end_of_game = False
 lives = 6
 
 print("Hangman Game")
 
 
-# Create blanks
+# Creating blanks for guessing words
+
 display = []
+
 for _ in range(word_length):
     display += "_"
 
+ #Game Brain
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
-
     if guess in display:
         print(f"You've already guessed {guess}")
 
     # Check guessed letter
     for position in range(word_length):
         letter = chosen_word[position]
-
         if letter == guess:
             display[position] = letter
 
     # Check if user is wrong.
     if guess not in chosen_word:
-
         print(f"You guessed {guess}, that's not in the word. You lose a life.")
-
         lives -= 1
         if lives == 0:
             end_of_game = True
@@ -46,8 +49,5 @@ while not end_of_game:
     if "_" not in display:
         end_of_game = True
         print("You win.")
-
-
-    from hangman_art import stages
-
+    #console
     print(stages[lives])
