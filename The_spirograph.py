a7ce6ddc4b86e52e8cb13bc582f0763140b6e2cf
Sabique Mattummal <119643024+sabique-mattummal@users.noreply.github.com>
2022-12-11 20:13:02 +0530

    Create The_spirograph.py

---
+++ The_spirograph.py
@@ -0,0 +1,27 @@
+import turtle
+import turtle as t
+import random
+
+tim = t.Turtle()
+t.colormode(255)
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
+
+
+def draw_spirograph(size_of_gap):
+    for i in range(int(360 / size_of_gap)):
+        tim.speed("fastest")
+        tim.color(random_color())
+        tim.circle(130)
+        tim.forward(5)
+        tim.setheading(tim.heading() + size_of_gap)
+
+draw_spirograph(5)
+
+screen = turtle.Screen()
+screen.exitonclick()
+
