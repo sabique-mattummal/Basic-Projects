179d208d38b16beaee733438e7d0e3b2bbeb18b4
Sabique Mattummal <119643024+sabique-mattummal@users.noreply.github.com>
2022-12-11 19:39:13 +0530

    Create turtle_hirst_painting.py

---
+++ turtle_hirst_painting.py
@@ -0,0 +1,40 @@
+import turtle as t
+from turtle import Screen
+import random
+
+color_list = color_list = [(202, 164, 109), (238, 240, 245),
+                           (150, 75, 49), (223, 201, 135), (52, 93, 124),
+                           (172, 154, 40), (140, 30, 19), (133, 163, 185),
+                           (198, 91, 71), (46, 122, 86), (72, 43, 35),
+                           (145, 178, 148), (13, 99, 71), (233, 175, 164),
+                           (161, 142, 158), (105, 74, 77), (55, 46, 50),
+                           (183, 205, 171), (36, 60, 74), (18, 86, 90),
+                           (81, 148, 129), (148, 17, 20), (14, 70, 64),
+                           (30, 68, 100), (107, 127, 153), (174, 94, 97),
+                           (176, 192, 209)]
+#Turtle initialisation
+
+scrren = Screen()
+t.colormode(255)
+tim = t.Turtle()
+tim.speed("fastest")
+tim.penup()
+tim.hideturtle()
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+
+#Turtle drawing
+
+number_of_dots = 100
+for i in range(1, number_of_dots + 1):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+
+    if i % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+scrren.exitonclick()
