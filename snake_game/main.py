f723acbdb389292d382377b49aedc66310047ee8
Sabique Mattummal <119643024+sabique-mattummal@users.noreply.github.com>
2022-12-12 12:06:46 +0530

    Create main.py

---
+++ snake_game/main.py
@@ -0,0 +1,54 @@
+from turtle import Turtle, Screen
+from snake import Snake
+from score_board import Scoreboard
+from food import Food
+import time
+
+#Screen setting
+
+screen = Screen()
+screen.setup(height=600, width=600)
+screen.bgcolor("#65647C")
+screen.title("Snake Game")
+screen.tracer(0)
+
+#import objects
+snake = Snake()
+food = Food()
+score_board = Scoreboard()
+
+#Controls
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+#Game initialisation
+is_game_on = True
+while is_game_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    #Food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        score_board.increase_score()
+
+    #Collision
+    if snake.head.xcor() > 280 or snake.head.ycor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() < -280:
+        is_game_on = False
+        score_board.game_over()
+
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            is_game_on = False
+            score_board.game_over()
+
+
+screen.exitonclick()
