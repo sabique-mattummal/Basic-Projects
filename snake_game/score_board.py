8eaf3e9eb84b9e73582d8695121211901c3f484d
Sabique Mattummal <119643024+sabique-mattummal@users.noreply.github.com>
2022-12-15 10:14:23 +0530

    Update score_board.py

--- snake_game/score_board.py
+++ snake_game/score_board.py
@@ -1,12 +1,16 @@
 from turtle import Turtle
 
-FONT = ("Arial", 22, "bold")
+FONT = ("Arial", 12, "bold")
 
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        
+        #storing the highest score into a txt file (Create a 'data.txt' in the same directory
+        with open( "data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(220, 250)
@@ -14,14 +18,17 @@ class Scoreboard(Turtle):
         self.update_score()
 
     def update_score(self):
-        self.write(arg=f"Score : {self.score}", align="center"
+        self.clear()
+        self.write(arg=f"Score : {self.score}/nHigh Score : {self.highr_score}", align="center"
                    , font=FONT)
-
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(arg=f"GAME OVER\nFinal Score : {self.score}",
-                   align="center", font=("Arial", 36, "italic"))
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open ("data.txt", mode ="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_score()
+        
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_score()
