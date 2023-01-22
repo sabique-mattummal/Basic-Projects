bf825fa9fa239d6714e69eb01889899303f34b91
Sabique Mattummal <119643024+sabique-mattummal@users.noreply.github.com>
2023-01-22 21:33:20 +0530

    Update main.py

--- Stock News(API-Twilio SMS)/main.py
+++ Stock News(API-Twilio SMS)/main.py
@@ -1,6 +1,14 @@
 from news import NewsData
 from stock import StockData
 from message import MessagePanel
+from twilio.rest import Client
+
+#Twilio Constants
+Account_sid = 'Your Twilio account sid'
+Auth_token = 'Your auth token)
+My_numb = 'Virual Twilio Number
+To_numb = 'To_num"
+
 
 stock_result = StockData()
 stock_result_value = stock_result.value()
@@ -9,7 +17,9 @@ message = f'Stock Alert\nIBM {stock_result_value}\n{news_data.title_1}' \
           f'\n{news_data.description_1}\nSource:{news_data.source_1}'
 
 if stock_result.percentage > 5:
-    MessagePanel.message_data(message)
+          client = Client(Account_sid, Auth_token)
+          message = client.messages.create(body=message, from=My_num, to=To_num) 
+ 
 else:
     print("No Alerts")
 
